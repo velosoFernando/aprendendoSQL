c43bbf0db089cad54bca8e2ad76353957c86787a
velosoFernando <fernandovelosomkt@gmail.com>
2025-11-16 04:33:05 -0300

    Aula 03 finalizada

---
+++ desafios/desafio 02/desafio 02.sql
@@ -0,0 +1,12 @@
+
+Descobrir quantos sobrenomes únicos nós temos na tabela person.person!
+
+<-->
+
+Utilizei a query:
+
+SELECT DISTINCT LastName
+FROM person.Person;
+
+Resultado: 1.216!
+
--- doc/03 DISTINCT & Desafios.sql
+++ doc/03 DISTINCT & Desafios.sql
@@ -5,3 +5,9 @@ Query:
 SELECT DISTINCT coluna1,coluna2
 FROM tabela
 
+Ao realizar uma consulta de FirstName é possível ver que o banco entrega mais de 19 mil users (ver imagem 4).
+
+Com o DISTINCT teremos pouco mais de 1k users (ver imagem 5).
+
+
+
